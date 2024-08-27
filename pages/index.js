5662ef02ddbd94f969e9c55f73e509208ee4010d
Felipe Lima <90412987+xLima12@users.noreply.github.com>
2024-08-27 03:23:51 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-  return <h1>Mamãe, eu amo você. Se você me ama deixa café pronto amanhã!!</h1>
+  return <h1>Mamãe, eu amo você. Se você me ama deixa café pronto amanhã!</h1>
 }
 
 export default Home;
