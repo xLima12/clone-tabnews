0b5f64f5a3ac148b9295665a7eb93818714d127a
Felipe Lima <90412987+xLima12@users.noreply.github.com>
2024-09-03 05:01:01 +0000

    finish `api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,34 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
-  response.status(200).json({ chave: "valor" });
+  const updatedAt = new Date().toISOString();
+
+  const getVersionDb = await database.query("SHOW server_version;");
+  
+  const version = getVersionDb.rows[0].server_version;
+
+  const getMaxConnections = await database.query("SHOW max_connections;");
+
+  const maxConnections = parseInt(getMaxConnections.rows[0].max_connections)
+
+  const dbName = process.env.POSTGRES_DB;
+
+  const getConectionUsed = await database.query({
+    text: "SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [dbName]
+  });
+  const conectionUsed = getConectionUsed.rows[0].count
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependecies: {
+      database: {
+        version: version,
+        max_connections: maxConnections,
+        connection_used: conectionUsed,
+      },
+    },
+  });
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,17 @@
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  expect(responseBody.updated_at).toBeDefined();
+
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+  
+  expect(responseBody.dependecies.database.version).toEqual("16.0");
+  
+  expect(responseBody.dependecies.database.max_connections).toEqual(100);
+  
+  expect(responseBody.dependecies.database.connection_used).toEqual(1);
+
 });
