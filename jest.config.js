e6d96d8e870a798285bc1c68fab1a6778f739c6a
Felipe Lima <90412987+xLima12@users.noreply.github.com>
2024-09-09 02:14:50 +0000

    add `/api/v1/migrations`

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+const dontenv = require("dotenv");
+dontenv.config({
+  path: ".env.development",
+})
+
+const nextJest = require("next/jest");
+
+const createJestConfig = nextJest({
+  dir: ".",
+});
+const jestConfig = createJestConfig({
+  moduleDirectories: ["node_modules", "<rootDir>"],
+});
+
+module.exports = jestConfig;
