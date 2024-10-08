67ae5769d02b26fe680b4e42fd11ef736da8940f
Felipe Lima <90412987+xLima12@users.noreply.github.com>
2024-10-08 19:09:41 -0300

    Revert "makes `npm test` more robust with `async-retry` and `orchestrator.js`"

--- jest.config.js
+++ jest.config.js
@@ -10,7 +10,6 @@ const createJestConfig = nextJest({
 });
 const jestConfig = createJestConfig({
   moduleDirectories: ["node_modules", "<rootDir>"],
-  testTimeout: 60000,
 });
 
 module.exports = jestConfig;
